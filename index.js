6b3fd50f5084dda99d954454a933b28357c59664
Coding-Abdi <abdi.brum@gmail.com>
2024-09-16 14:32:26 +0100

    introduced arrays

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let firstCard = 0;
-let secondCard = 0;
-let anotherCard = 0;
-let sum = 0;
+let firstCard = Math.floor(Math.random() * 10) + 2;
+let secondCard = Math.floor(Math.random() * 10) + 2;
+let cards = [  firstCard, secondCard ]
+let sum = firstCard + secondCard;
 
 let hasBlackjack = false;
 let isAlive = true
@@ -10,19 +10,19 @@ let msgEl = document.getElementById("msg-el")
 let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
 
+function startGame() {
+    play();
+}
 function play() {
-    firstCard = Math.floor(Math.random() * 10) + 2;
-    secondCard = Math.floor(Math.random() * 10) + 2;
-    sum = firstCard + secondCard;
+    cardsEl.textContent = "Cards: " + cards[0] + " & " + cards[1];
+    sumEl.textContent = "Sum: " + sum; 
     console.log(sum)
-    cardsEl.textContent = "Cards: " + firstCard + " & " + secondCard;
-    sumEl.textContent = "Sum: " + sum;    
     if (sum <= 20) {
         msgEl.textContent = "Do you want to draw a new card?";
-} else if (sum === 21) {
+    } else if (sum === 21) {
     msgEl.textContent = "You've got a Blackjack"
     hasBlackjack = true;
-} else {
+    } else {
     msgEl.textContent = "Game over!"
     isAlive = false;
 }
@@ -31,19 +31,19 @@ console.log("You are still in the game" + " = " + isAlive)
 }
 
 function newCard() {
-    anotherCard = Math.floor(Math.random() * 10) + 2;
-    sum += anotherCard;
+    card = 5;
+    sum += card;
     play();
 }
 
-function restart() {
-    msgEl.textContent = "Want to play a round?";
-    cardsEl.textContent = "Cards:";
-    sumEl.textContent = "Sum:";
-    firstCard = 0;
-    secondCard = 0;
-    sum = 0;
-}
+// function restart() {
+//     msgEl.textContent = "Want to play a round?";
+//     cardsEl.textContent = "Cards:";
+//     sumEl.textContent = "Sum:";
+//     firstCard = 0;
+//     secondCard = 0;
+//     sum = 0;
+// }
 
 // if (sum <= 20) {
 //     console.log("Do you want to draw a new card?")
